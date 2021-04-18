98724ff90ad4b598f1d002a6adcf562b6ce2a572
Sebastian Riedel <git@basxto.de>
2021-04-18 22:33:52 +0200

    Bounce ball

--- src/game.c
+++ src/game.c
@@ -1,5 +1,66 @@
+#include <gb/gb.h>
+#include <stdbool.h>
 #include "game.h"
 
-void load_level(uint8_t level){
 
+// has position (x,y) and direction (dx,dy)
+// if there is a block on left and right and ball 16x16, we have x max 128
+struct {
+    uint8_t x;
+    int8_t dx;
+    uint8_t y;
+    int8_t dy;
+} ball;
+
+// move and collide
+void move_ball(){
+    bool mirror_v = false, mirror_h = false;
+    // bounce at screen borders
+    if((uint8_t)ball.x < ball.dx){
+        mirror_h = true;
+        ball.x = 0;
+    }else if(ball.x - ball.dx > 160-16){
+        mirror_h = true;
+        ball.x = 160-16;
+    }
+    if((uint8_t)ball.y < ball.dy){
+        mirror_v = true;
+        ball.y = 0;
+    }else if(ball.y - ball.dy > 144-16){
+        mirror_v = true;
+        ball.y = 144-16;
+    }
+    if(mirror_h)
+        ball.dx *= -1;
+    if(mirror_v)
+        ball.dy *= -1;
+    ball.x -= ball.dx;
+    ball.y -= ball.dy;
+}
+
+// render the struct to a sprite
+void render_ball(){
+    move_sprite(0, ball.x+8, ball.y+16);
+    move_sprite(1, ball.x+16, ball.y+16);
+    move_sprite(2, ball.x+8, ball.y+24);
+    move_sprite(3, ball.x+16, ball.y+24);
+}
+
+// set up game specific interrupts and such
+void init_game(){
+
+}
+
+// load the blocks and such
+void load_level(uint8_t level){
+    ball.x = 31;
+    ball.dx = 1;
+    ball.y = 33;
+    ball.dy = 2;
+    while(level == 0){
+        move_ball();
+        render_ball();
+        wait_vbl_done();
+        wait_vbl_done();
+    }
 }
--- src/main.c
+++ src/main.c
@@ -8,11 +8,20 @@ const char text_press_start[] = "PRESS START!";
 void init(){
     // init game elements
     set_sprite_data(0x20, great_burst_fg_size, great_burst_fg_data);
+
+    set_sprite_prop(0,S_PALETTE);
+    set_sprite_tile(0,0x20);
+    set_sprite_tile(1,0x21);
+    set_sprite_tile(2,0x22);
+    set_sprite_tile(3,0x23);
     set_bkg_data(0x80, great_burst_blocks_size, great_burst_blocks_data);
     // init menu font
     set_bkg_1bit_data(0x20, squont8ng_size, squont8ng_data, 3);
-    set_bkg_tiles(0, 0, strlen(text_press_start), 1, text_press_start);
+    set_bkg_tiles(0, 0, sizeof(text_press_start)-1, 1, text_press_start);
+    // set dmg palette for shiny part
+    OBP1_REG = 0x26;
     SHOW_BKG;
+    SHOW_SPRITES;
 }
 
 
